c04fa5f771d70eb82201e976d245d3c865116065
molchanovchess <alexmolchanovchess@gmail.com>
2022-06-28 12:23:24 +0300

    adding files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ContactAppComponent } from './components/contact-app/contact-app.component';
+import { ContactListComponent } from './components/contact-list/contact-list.component';
+import { ContactCardComponent } from './components/contact-card/contact-card.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactAppComponent,
+    ContactListComponent,
+    ContactCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/contact-list/contact-list.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Contact } from 'src/app/models/Contact';
+import { ContactService } from 'src/app/services/contact.service';
+
+@Component({
+  selector: 'app-contact-list',
+  templateUrl: './contact-list.component.html',
+  styleUrls: ['./contact-list.component.css']
+})
+export class ContactListComponent implements OnInit {
+  public contacts: Contact[] = [] as Contact[];
+  public errorMessage : string | undefined = undefined;
+
+  @Output() sendContact = new EventEmitter();
+
+  constructor(private contactService: ContactService) { }
+
+  ngOnInit(): void {
+    this.contactService.getAllContacts().subscribe((data: Contact[]) => {
+      this.contacts = data;
+    }, (error) => {
+      this.errorMessage = error;
+    });
+  }
+
+  selectContact(contact : Contact) {
+    this.sendContact.emit(contact);
+  }
+
+}
